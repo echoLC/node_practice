8892ad5fd738e77324c1ae0105341eb2e5180967
luochao <luochao@yy.com>
2020-06-11 17:48:44 +0800

    feat: reduce实现compose

--- ex02/index.js
+++ ex02/index.js
@@ -1,13 +1,3 @@
 module.exports.compose = middlewares => {
-    return function () {
-        return dispatch(0)
-        function dispatch(i) {
-            // ##BEGIN## 代码已加密
-            let middleware = middlewares[i]
-            if (!middleware) return Promise.resolve()
-            return Promise.resolve(middleware(function next() {
-                return dispatch(i + 1)
-            }))
-        }
-    }
+    return () => middlewares.reduceRight( (acc, cur) => () => Promise.resolve(cur(acc)), () => {})()
 }
